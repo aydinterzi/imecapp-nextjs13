53894585ede9f084159ef99917479a44ab582aae
Aydın Terzi <aydinterzi7@gmail.com>
2023-03-07 23:41:26 +0300

    filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,44 @@
+import Filter from '@/components/Filter'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div><Filter options={
+      [
+        { id: '1', value: 'test' },
+        { id: '2', value: 'test2' },
+        { id: '3', value: 'test3' },
+        { id: '4', value: 'test4' },
+        { id: '5', value: 'test5' },
+      ]
+    } />
+      <Filter options={
+        [
+          { id: '1', value: 'test' },
+          { id: '2', value: 'test2' },
+          { id: '3', value: 'test3' },
+          { id: '4', value: 'test4' },
+          { id: '5', value: 'test5' },
+        ]
+      } />
+      <Filter options={
+        [
+          { id: '1', value: 'test' },
+          { id: '2', value: 'test2' },
+          { id: '3', value: 'test3' },
+          { id: '4', value: 'test4' },
+          { id: '5', value: 'test5' },
+        ]
+      } />
+      <Filter options={
+        [
+          { id: '1', value: 'test' },
+          { id: '2', value: 'test2' },
+          { id: '3', value: 'test3' },
+          { id: '4', value: 'test4' },
+          { id: '5', value: 'test5' },
+        ]
+      } /></div>
   )
 }
 
--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from 'react'
 
 type Filter = {
@@ -8,18 +9,30 @@ type Filter = {
 
 type Props = {
   options: Filter[]
-  onApply: (value: Filter[]) => void,
-  title: string
+  onApply?: (value: Filter[]) => void,
+  title?: string
 }
 
-const Filter = ({options, onApply, title }: Props) => {
+const Filter = ({ options, onApply, title }: Props) => {
 
-  const [filters, setFilters] = useState<{ id:string, value:string, selected:boolean}[]>(options.map((option) => ({ ...option, selected: false })))
-
- 
+  const [filters, setFilters] = useState<{ id: string, value: string, selected: boolean }[]>(options.map((option) => ({ ...option, selected: false })))
 
   return (
-    <div>Filter</div>
+    <div className="dropdown">
+      <label tabIndex={0} className="btn m-1">Filter</label>
+      <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+        {filters.map((filter) => (
+          <li key={filter.id}>
+            <div className="form-control">
+              <label className="label cursor-pointer">
+                <span className="label-text">{filter.value}</span>
+                <input type="checkbox" checked className="checkbox" />
+              </label>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
